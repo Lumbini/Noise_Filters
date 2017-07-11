35fa8e884a08fcd672370aeb7220e89da6dbc441
Lumbini Parnas <lparnas@wpi.edu>
2017-07-11 16:22:30 -0400

    Added helper funtions to Temporal Median Filter

--- TestTemporalMedianFilter.py
+++ TestTemporalMedianFilter.py
@@ -4,106 +4,94 @@ from TemporalMedianFilter import TemporalMedianFilter
 
 class TestTemporalMedianFilter(unittest.TestCase):
 
+	# Help set up the test, can be repeated several time swith addiontional inputs
+	def helpSetUp(self, D, input1, expected1, *therest):
+		filter = TemporalMedianFilter(D)
+		self.assertEqual(filter.update(input1), expected1)
+
+	# Test with D=3, 1st scan only
 	def testFirstScan(self):
-		filter = TemporalMedianFilter(1)
 		input1 = [0., 1., 2., 1., 3.]
 		expected1 = [0., 1., 2., 1., 3.]
-		self.assertEqual(filter.update(input1), input1)
+		self.helpSetUp(3, input1, expected1)
 
+	# Test with D=3, First 2 scans
 	def testFirstTwoScans(self):
-		filter = TemporalMedianFilter(3)
 		input1 = [0., 1., 2., 1., 3.]
 		expected1 = [0., 1., 2., 1., 3.]
-		self.assertEqual(filter.update(input1), expected1)
 		input2 = [1., 5., 7., 1., 3.]
 		expected2 = [0.5, 3., 4.5, 1., 3.]
-		self.assertEqual(filter.update(input2), expected2)
+		self.helpSetUp(3, input1, expected1, input2, expected2)
 
+	# Test with D=3, First 3 scans
 	def testFirstThreeScans(self):
-	    filter = TemporalMedianFilter(3)
-	    input1 = [0., 1., 2., 1., 3.]
-	    expected1 = [0., 1., 2., 1., 3.]
-	    self.assertEqual(filter.update(input1), expected1)
-	    input2 = [1., 5., 7., 1., 3.]
-	    expected2 = [0.5, 3., 4.5, 1., 3.]
-	    self.assertEqual(filter.update(input2), expected2)
-	    input3 = [2., 3., 4., 1., 0.]
-	    expected3 = [1., 3., 4., 1., 3.]
-	    self.assertEqual(filter.update(input3), expected3)
-
-	def testFirstFourScans(self):
-	    filter = TemporalMedianFilter(3)
-	    input1 = [0., 1., 2., 1., 3.]
-	    expected1 = [0., 1., 2., 1., 3.]
-	    self.assertEqual(filter.update(input1), expected1)
-	    input2 = [1., 5., 7., 1., 3.]
-	    expected2 = [0.5, 3., 4.5, 1., 3.]
-	    self.assertEqual(filter.update(input2), expected2)
-	    input3 = [2., 3., 4., 1., 0.]
-	    expected3 = [1., 3., 4., 1., 3.]
-	    self.assertEqual(filter.update(input3), expected3)
-	    input4 = [3., 3., 3., 1., 3.]
-	    expected4 = [1.5, 3., 3.5, 1., 3.]
-	    self.assertEqual(filter.update(input4), expected4)
-
-	# note that this is the point at which the first scan starts to be ignored by the temporal filte
-	def testFirstFiveScans(self):
-	    filter = TemporalMedianFilter(3)
-	    input1 = [0., 1., 2., 1., 3.]
-	    expected1 = [0., 1., 2., 1., 3.]
-	    self.assertEqual(filter.update(input1), expected1)
-	    input2 = [1., 5., 7., 1., 3.]
-	    expected2 = [0.5, 3., 4.5, 1., 3.]
-	    self.assertEqual(filter.update(input2), expected2)
-	    input3 = [2., 3., 4., 1., 0.]
-	    expected3 = [1., 3., 4., 1., 3.]
-	    self.assertEqual(filter.update(input3), expected3)
-	    input4 = [3., 3., 3., 1., 3.]
-	    expected4 = [1.5, 3., 3.5, 1., 3.]
-	    self.assertEqual(filter.update(input4), expected4)
-	    input5 = [10., 2., 4., 0., 0.]
-	    expected5 = [2.5, 3., 4., 1., 1.5]
-	    self.assertEqual(filter.update(input5), expected5)
-
-	def testSixScans(self):
-		filter = TemporalMedianFilter(3)
 		input1 = [0., 1., 2., 1., 3.]
 		expected1 = [0., 1., 2., 1., 3.]
-		self.assertEqual(filter.update(input1), expected1)
 		input2 = [1., 5., 7., 1., 3.]
 		expected2 = [0.5, 3., 4.5, 1., 3.]
-		self.assertEqual(filter.update(input2), expected2)
 		input3 = [2., 3., 4., 1., 0.]
 		expected3 = [1., 3., 4., 1., 3.]
-		self.assertEqual(filter.update(input3), expected3)
+		self.helpSetUp(3, input1, expected1, input2, expected2, input3, expected3)
+
+	# Test with D=3, First 4 scans
+	def testFirstFourScans(self):
+		input1 = [0., 1., 2., 1., 3.]
+		expected1 = [0., 1., 2., 1., 3.]
+		input2 = [1., 5., 7., 1., 3.]
+		expected2 = [0.5, 3., 4.5, 1., 3.]
+		input3 = [2., 3., 4., 1., 0.]
+		expected3 = [1., 3., 4., 1., 3.]
 		input4 = [3., 3., 3., 1., 3.]
 		expected4 = [1.5, 3., 3.5, 1., 3.]
-		self.assertEqual(filter.update(input4), expected4)
-		input5 = [10., 2., 4., 0., 0.]
-		expected5 = [2.5, 3., 4., 1., 1.5]
-		self.assertEqual(filter.update(input5), expected5)
-		input6 = [21., 3., 1., 7., 4.]
-		expected6 = [6.5, 3., 3.5, 1., 1.5]
-		self.assertEqual(filter.update(input6), expected6)
+		self.helpSetUp(3, input1, expected1, input2, expected2, input3, expected3, input4, expected4)
 
-
-	def testFiveScansWithDifferenceD(self):
-		filter = TemporalMedianFilter(2)
+	# Test with D=3, First 5 scans
+	# note that this is the point at which the first scan gets ignored by the temporal median filter
+	def testFirstFiveScans(self):
 		input1 = [0., 1., 2., 1., 3.]
 		expected1 = [0., 1., 2., 1., 3.]
-		self.assertEqual(filter.update(input1), expected1)
 		input2 = [1., 5., 7., 1., 3.]
 		expected2 = [0.5, 3., 4.5, 1., 3.]
-		self.assertEqual(filter.update(input2), expected2)
 		input3 = [2., 3., 4., 1., 0.]
 		expected3 = [1., 3., 4., 1., 3.]
-		self.assertEqual(filter.update(input3), expected3)
+		input4 = [3., 3., 3., 1., 3.]
+		expected4 = [1.5, 3., 3.5, 1., 3.]
+		input5 = [10., 2., 4., 0., 0.]
+		expected5 = [2.5, 3., 4., 1., 1.5]
+		self.helpSetUp(3, input1, expected1, input2, expected2, input3, expected3, input4, expected4, input5, expected5)
+
+	# Test with D=3, First 6 scans
+	# note that this is the point at which the first 2 scans gets ignored by the temporal median filter
+	def testSixScans(self):
+		input1 = [0., 1., 2., 1., 3.]
+		expected1 = [0., 1., 2., 1., 3.]
+		input2 = [1., 5., 7., 1., 3.]
+		expected2 = [0.5, 3., 4.5, 1., 3.]
+		input3 = [2., 3., 4., 1., 0.]
+		expected3 = [1., 3., 4., 1., 3.]
+		input4 = [3., 3., 3., 1., 3.]
+		expected4 = [1.5, 3., 3.5, 1., 3.]
+		input5 = [10., 2., 4., 0., 0.]
+		expected5 = [2.5, 3., 4., 1., 1.5]
+		input6 = [21., 3., 1., 7., 4.]
+		expected6 = [6.5, 3., 3.5, 1., 1.5]
+		self.helpSetUp(2, input1, expected1, input2, expected2, input3, expected3, input4, expected4, input5, expected5, input6, expected6)
+
+	# Test with D=2, First 5 scans
+	# The first 2 scans get ignored by the temporal median filter
+	def testFiveScansWithDifferenceD(self):
+		input1 = [0., 1., 2., 1., 3.]
+		expected1 = [0., 1., 2., 1., 3.]
+		input2 = [1., 5., 7., 1., 3.]
+		expected2 = [0.5, 3., 4.5, 1., 3.]
+		input3 = [2., 3., 4., 1., 0.]
+		expected3 = [1., 3., 4., 1., 3.]
 		input4 = [3., 3., 3., 1., 3.]
 		expected4 = [2., 3., 4., 1., 3.]
-		self.assertEqual(filter.update(input4), expected4)
 		input5 = [10., 2., 4., 0., 0.]
 		expected5 = [3., 3., 4., 1., 0.]
-		self.assertEqual(filter.update(input5), expected5)
+		self.helpSetUp(3, input1, expected1, input2, expected2, input3, expected3, input4, expected4, input5, expected5)
+
 
 # Runs tests and displays the details of the results
 suite = unittest.TestLoader().loadTestsFromTestCase(TestTemporalMedianFilter)
